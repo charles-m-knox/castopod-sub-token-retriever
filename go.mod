62eb6d1f262d815a7d773050ebd383c2f8e61e27
cmcode <code@cmcode.dev>
2024-08-21 11:55:32 -0700

    fixes to go mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module git.cmcode.dev/cmcode/castopod-sub-token-retriever
 
 go 1.23.0
+
+require git.cmcode.dev/cmcode/uuid v0.0.1
--- pkg/lib/lib.go
+++ pkg/lib/lib.go
@@ -3,6 +3,9 @@ package cstr
 import (
 	"fmt"
 	"net/smtp"
+	"strings"
+
+	uuid "git.cmcode.dev/cmcode/uuid"
 )
 
 // SendEmail sends an email to the desired recipient.
@@ -19,4 +22,11 @@ func SendEmail(to []string, subject, body string, from string, password string, 
 	}
 
 	return nil
+}
+
+// NewUUID returns a double uuid with the hyphens removed, leading to a
+// 64-character string. Castopod appears to use something like this for its
+// token.
+func NewUUID() string {
+	return strings.ReplaceAll(fmt.Sprintf("%v%v", uuid.New(), uuid.New()), "-", "")
 }
